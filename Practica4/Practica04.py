f0d3c64b666636b5f169d6d39d65a5748b944bfe
miguel <josemigueltr@ciencias.unam.mx>
2019-10-09 16:08:22 -0500

    Problemas arreglados Arreglé los problemas de identación,borre los directorios de imagenes de hombre y mujer

--- Practica4/Practica04.py
+++ Practica4/Practica04.py
@@ -20,6 +20,8 @@ import random
 # convierte objetos de Python en una secuencia de bytes
 import pickle
 
+import matplotlib.pyplot as plt 
+
 
  #Clase que obtiene imagenes entre un directorio y las separa
 class Preprocess:
@@ -30,6 +32,7 @@ class Preprocess:
         self.training_data = []
         #almacena las imagenes
         self.features = []
+        #Almacena
         self.subdirectories = []
         #almacena las etiquetas
         self.labels = []
@@ -40,6 +43,7 @@ class Preprocess:
          #iteramos sobre cada elemento de la carpeta
         for sub in os.listdir(Directory):
             path = os.path.join(Directory,sub)
+            
             #si es un directorio lo anexamos ala lista para procesarlo despues
             if os.path.isdir(path):
                 self.subdirectories.append(path)
@@ -51,17 +55,15 @@ class Preprocess:
      #Obtiene las imagenes de cada uno de los subdirectorios
     def load_training_data(self):
         for category in self.subdirectories:
-            print("Estoy procesando imagenes de ${category")
             path = category
             class_num = self.subdirectories.index(category)
             for img in os.listdir(path):
                 try:
                     ##Se modifica la imagen a una escala de grises
-	                img_array = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
+                    img_array = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
                     #Se modifica el tamaño de la imagen
-	                new_array = cv2.resize(img_array,(self.IMG_SIZE,self.IMG_SIZE))
-                     #Agregamos la imagen junto con su etiqueta al trainig Data
-	                self.training_data.append([new_array,class_num])
+                    new_array = cv2.resize(img_array,(self.IMG_SIZE,self.IMG_SIZE))
+                    self.training_data.append([new_array,class_num])
                 except Exception as e:
 	                pass
 
@@ -70,26 +72,50 @@ class Preprocess:
     def split_and_prepare(self):
         random.shuffle(self.training_data)
         for features,label in self.training_data:
-	        self.features.append(features)
-	        self.labels.append(label)
+            self.features.append(features)
+            self.imprime(features,label)
+            self.labels.append(label)
+           
+         
         self.features = np.array(self.features).reshape(-1,self.IMG_SIZE, self.IMG_SIZE,1)
         self.features = self.features/255.0
 
 
     #Almacenamos cada objeto de los arreglos features y labels en un archivo de bytes
     def write_out(self):
-	    pickle_out = open("X.pickle","wb")
-	    pickle.dump(self.features,pickle_out)
-	    pickle_out.close()
+        pickle_out = open("X.pickle","wb")
+        pickle.dump(self.features,pickle_out)
+        pickle_out.close()
+        pickle_out = open("Y.pickle","wb")
+        pickle.dump(self.labels,pickle_out)
+        pickle_out.close()
 
-	    pickle_out = open("Y.pickle","wb")
-	    pickle.dump(self.labels,pickle_out)
-	    pickle_out.close()
-
-
-
-
-
+    
+    def imprime(self,i, titulo):
+        plt.figure()
+        plt.title(titulo)
+        plt.imshow(i)
+        plt.colorbar()
+        plt.grid(False)
+        plt.show()
+    
+    def title(self):
+        for lab in self.labels:
+            print(lab)
+            
+p=Preprocess()
+p.get_subdirectories("./Practica4/Data")
+p.load_training_data()
+p.split_and_prepare()
+p.title()
+# p.split_and_prepare()
+# p.write_out()
+#
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
 
 
 #%%
