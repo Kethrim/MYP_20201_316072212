47660085b472f1428e30aba8b186221592ef3b8b
Kethrim <kethrim@ciencias.unam.mx>
2019-10-13 14:10:28 -0500

    Pruebas del calsificador jirafa, falta situarlo en la carpeta Pruebas/

--- test_ClasificadorJirafa.py
+++ test_ClasificadorJirafa.py
@@ -5,12 +5,12 @@ class test_ClasificadorJirafa(unittest.TestCase):
 
     def test_EsJirafa(self):
         id = jirafa()
-        self.assertTrue(id.esJirafa("test_Imagenes/jirafa_prueba.jpg"))
-        self.assertTrue(id.esJirafa("test_Imagenes/jirafa2_prueba.jpg"))
-        self.assertFalse(id.esJirafa("test_Imagenes/avestruz_prueba.jpeg"))
-        self.assertFalse(id.esJirafa("test_Imagenes/cuyo_prueba.jpg"))
-        self.assertFalse(id.esJirafa("test_Imagenes/hipo_prueba.jpg"))
-        self.assertFalse(id.esJirafa("test_Imagenes/pingu_prueba.jpg"))
+        self.assertTrue(id.esJirafa("Pruebas/test_Imagenes/jirafa_prueba.jpg"))
+        self.assertTrue(id.esJirafa("Pruebas/test_Imagenes/jirafa2_prueba.jpg"))
+        self.assertFalse(id.esJirafa("Pruebas/test_Imagenes/avestruz_prueba.jpeg"))
+        self.assertFalse(id.esJirafa("Pruebas/test_Imagenes/cuyo_prueba.jpg"))
+        self.assertFalse(id.esJirafa("Pruebas/test_Imagenes/hipo_prueba.jpg"))
+        self.assertFalse(id.esJirafa("Pruebas/test_Imagenes/pingu_prueba.jpg"))
 
 if __name__ == "__main__":
     uniitest.main()
