3b9cc0c0c4a14de33e7849723ff608a5c3cea6fd
miguel <josemigueltr@ciencias.unam.mx>
2019-10-13 12:11:37 -0500

    pruebas listas

--- pruebas/hippo_classifierTest.py
+++ pruebas/hippo_classifierTest.py
@@ -1,30 +1,35 @@
-
+#%%
 import unittest
 from clasificador.hippo_classifier import HippoClassifier 
 
+#Clase de pruebas unitarias de la clase hippo_classifier.py
 class hippo_prueba(unittest.TestCase):
     
-    def load_model(self):
-        hipo=HippoClassifier("hgggg")
+    #REvisa ue se cargue correctamente el modelo
+    def test_load_model(self):
+        hipo=HippoClassifier("Ruta invalida")
         self.assertFalse(hipo.model)
     
-    def is_not_hipo(self):
+    #revisa si el modelo detecta que le contenido de una imagen no es un hipoptamo
+    def test_is_not_hipo(self):
         hipo=HippoClassifier("modelos/hippo_model.h5")
         self.assertFalse(hipo.is_hippo('revision/141.perro2.jpg'))
     
-    
-    def is_hipo(self):
+   #revisa si el modelo detecta que le contenido de una imagen es un hipopotamo
+    def test_is_hipo(self):
         hipo=HippoClassifier("modelos/hippo_model.h5")
-        self.assertEqual(True,self.assertTrue(hipo.is_hippo('revision/1.153-204034-hippo-dead-river-south-ethiopia_700x400.jpeg')))
+        self.assertEqual(True,hipo.is_hippo('revision/1.153-204034-hippo-dead-river-south-ethiopia_700x400.jpeg'))
         
-    
-    def load_image(self):
+    #Revisa que se cache el error cuando se introduce una imagen erronea
+    def test_load_image(self):
         hipo=HippoClassifier("modelos/hippo_model.h5")
-        a=hipo.pre_img("pruebas")
-        self.assertTrue(len(a)!=0)
+        a=hipo.pre_img("Ruta invalida")
+        self.assertTrue(len(a)==0)
  
 
 if __name__ == '__main__':
     unittest.main()
 
 
+
+#%%
