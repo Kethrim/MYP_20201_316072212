d29d43cce651a431907f12d728c38e69cd329799
Kethrim <kethrim@ciencias.unam.mx>
2019-10-14 23:33:15 -0500

    Agregué una clase al archivo, faltan los modelos de pinguino y avestruz.

--- Main.py
+++ Main.py
@@ -3,22 +3,44 @@ from clasificador.hippo_classifier import HippoClassifier as clas_hipo
 from clasificador.clasificadorCuyo import ClasificadorCuyo as clas_cuyo
 from clasificador.clasificadorJirafa import clasificadorJirafa as clas_jirf
 
-hipo = clas_hipo('modelos/hippo_model.h5')
-cuyo = clas_cuyo()
-jirf = clas_jirf()
+class clasificador():
+  """
+  Clase que clasifica una imagen en:
+  hipopótamo
+  pinguino
+  avestruz
+  jirafa
+  cuyo
+  otro.
+  """
+  
+  def __init__(self):
+  """
+  Se cargan los modelos previamente creados de cada animal.
+  """
+  try:
+    hipo = clas_hipo('modelos/hippo_model.h5')
+    cuyo = clas_cuyo()
+    jirf = clas_jirf()    
+  except Exception as e:
+    print("No se pueden cargar los modelos.")
+
+
+  def queAnimalEs(self,ruta):
+    if hipo.is_hippo(ruta):
+      return 'Es un hipopotamo!'
+    elif False:  
+      return 'Es un pinguino!'
+    elif False:
+      return 'Es un avestruz!'
+    elif cuyo.es_cuyo(ruta):
+      return 'Es un cuyo!'
+    elif jirf.esJirafa(ruta):
+      return 'Es una jirafa!'
+    else:
+      return 'No es nada'
+
 
 ruta = sys.argv[1]
-
-if hipo.is_hippo(ruta):
-  # falla con revision/cuyo_prueba.jpg, dice que es un hipo
-  print('Es un hipopotamo!')
-elif False:  
-  print('Pinguino')
-elif False:
-  print('Ave..')
-elif cuyo.es_cuyo(ruta):
-  print('Es un cuyo!')
-elif jirf.esJirafa(ruta):
-  print('Es una jirafa!')  
-else:
-  print('No es nada')
+c = clasificador()
+print(c.queAnimalEs(ruta))
